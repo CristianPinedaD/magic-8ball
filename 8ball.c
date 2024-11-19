50fa39155eb7bc74e8ca27cd44b57a89fac5f2ec
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-19 10:46:48 -0500

    Create main() and add includes

--- 8ball.c
+++ 8ball.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+int main(void) {
+	char *positiveResponses[10] = {""};
+
+	char *ambiguousResponses[5] = {""};
+
+	char *negativeResponses[5] = {""};
+
+	int tryAgain = 1; 
+
+    while (tryAgain == 1) {
+
+    }
+}
