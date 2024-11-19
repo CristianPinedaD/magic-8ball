10c4d47d91ba3cd0dc54bd6d265299e32e7ea6fc
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-19 11:29:19 -0500

    Made response array constant

--- 8ball.c
+++ 8ball.c
@@ -5,17 +5,17 @@
 char *determineAnswer(int);
 int calculateRandom();
 
-char *positiveResponses[10] = {
+const char *positiveResponses[10] = {
 	"It is certain",	 "It is decidedly so", "Without a doubt",
 	"Yes definitely",	 "You may rely on it", "As I see it, yes",
 	"Most likely",		 "Outlook good",	   "Yes",
 	"Signs point to yes"};
 
-char *ambiguousResponses[5] = {"Reply hazy, try again", "Ask again later",
+const char *ambiguousResponses[5] = {"Reply hazy, try again", "Ask again later",
 							   "Better not tell you now", "Cannot predict now",
 							   "Concentrate and ask again"};
 
-char *negativeResponses[5] = {"Don't count on it", "My reply is no",
+const char *negativeResponses[5] = {"Don't count on it", "My reply is no",
 							  "My sources say no", "Outlook not so good",
 							  "Very doubtful"};
 
